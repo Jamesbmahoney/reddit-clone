8d6853739a256fea254c76d8d5d11246387e8017
James Barry <jbm03003@gmail.com>
2022-10-12 14:17:06 -0400

    added create post to postbox

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -22,8 +22,12 @@ const {
   formState: { errors },
 } = useForm<FormData>()
 
+const onSubmit = handleSubmit(async (formData) => {
+  console.log(formData)
+})
+
   return (
-    <form className='sticky top-16 z-50 rounded-md border border-gray-300 bg-white p-2'>
+    <form onSubmit={onSubmit} className='sticky top-16 z-50 rounded-md border border-gray-300 bg-white p-2'>
         <div className='flex items-center space-x-3'>
             <Avatar />
 
@@ -56,7 +60,7 @@ const {
             <div className='flex items-center px-2'>
               <p className='min-w-[90px]'>Subreddit:</p>
               <input className='m-2 flex-1 bg-blue-50 p-2 outline-none' 
-                {...register('subreddit')}
+                {...register('subreddit', { required : true})}
                 type="text"
                 placeholder="i.e. reactjs"
               />
@@ -71,6 +75,27 @@ const {
                 placeholder="Optional..."
               />
             </div>
+            )}
+
+            {/* Errors */}
+            {Object.keys(errors).length > 0 && (
+              <div className='space-y-2 p-2 text-red-500'>
+                {errors.postTitle?.type === 'required'  && (
+                  <p>A Post Is Required</p>
+                )}
+
+                {errors.subreddit?.type === 'required'  && (
+                  <p>A Subreddit Is Required</p>
+                )}
+              </div>
+            )}
+
+            {!!watch('postTitle') && (
+              <button 
+                type='submit' 
+                className='w-full rounded-full bg-blue-400 p-2 text-white'>
+                Create Post
+              </button>
             )}
           </div>
         )}
